0c17d8c853a5500eea5bfcb2691b9afafcbddb71
Saúl Ortega <saulortega@users.noreply.github.com>
2018-03-28 10:23:56 -0500

    Create lseg.go

---
+++ lseg.go
@@ -0,0 +1,41 @@
+package pgeo
+
+import (
+	"database/sql/driver"
+	"errors"
+	"fmt"
+)
+
+type Lseg [2]Point
+
+func (l Lseg) Value() (driver.Value, error) {
+	return valueLseg(l)
+}
+
+func (l *Lseg) Scan(src interface{}) error {
+	return scanLseg(l, src)
+}
+
+func valueLseg(l Lseg) (driver.Value, error) {
+	return fmt.Sprintf(`[%s]`, formatPoints(l[:])), nil
+}
+
+func scanLseg(l *Lseg, src interface{}) error {
+	if src == nil {
+		*l = NewLseg(Point{}, Point{})
+		return nil
+	}
+
+	points, err := parsePointsSrc(src)
+	if err != nil {
+		return err
+	}
+
+	if len(points) != 2 {
+		return errors.New("wrong lseg")
+	}
+
+	*l = NewLseg(points[0], points[1])
+
+	return nil
+}
